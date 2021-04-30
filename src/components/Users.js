3d31024b7f46c43575700c9b310cc03a7d5dee41
pnek <porysokheng@gmail.com>
2021-05-01 01:16:57 +0700

    Add spinner gif

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import Spinner from './Spinner';
 import UserItem from './UserItem';
 
-const Users = ({ users }) => {
+const Users = ({ users, loading }) => {
   const displayStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3,1fr)',
     gridGap: '1rem',
   };
 
-  console.log(users);
+  console.log(loading);
 
-  return (
-    <div style={displayStyle}>
-      {users.map((user) => {
-        return <UserItem user={user} key={user.id} />;
-      })}
-    </div>
-  );
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={displayStyle}>
+        {users.map((user) => {
+          return <UserItem user={user} key={user.id} />;
+        })}
+      </div>
+    );
+  }
 };
 
 export default Users;
