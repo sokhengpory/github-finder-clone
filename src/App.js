b13fb717194b4258bdc5b27b164acf3cc49bb7a4
pnek <porysokheng@gmail.com>
2021-05-01 01:16:35 +0700

    Add spinner gif

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import Users from './components/Users';
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getUsers = async (username) => {
+    setLoading(true);
     const res = await axios.get(
       `https://api.github.com/search/users?q=${username}`
     );
     setUsers(res.data.items);
-    console.log(users);
+    setLoading(false);
   };
 
   return (
@@ -21,7 +23,7 @@ const App = () => {
       <Navbar />
       <div className="container">
         <Search getUsers={getUsers} />
-        <Users users={users} />
+        <Users users={users} loading={loading} />
       </div>
     </div>
   );
